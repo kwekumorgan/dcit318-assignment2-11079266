c15e6f5d2e002d0249ae548e8dcb4a4955864680
kwekumorgan <morganmiguel.30@gmail.com>
2025-08-08 16:41:31 +0000

    Completed interface application

---
+++ Interfaces/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    internal class Program
+    {
+        public interface IMovable
+        {
+             void Move();
+        }
+
+        public class Car : IMovable
+        {
+            public void Move()
+            {
+                Console.WriteLine("Car is Moving");
+            }
+        }
+
+        public class Bicycle : IMovable
+        {
+            public void Move()
+            {
+                Console.WriteLine("Bicycle is Moving");
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            Car BMW = new Car();
+
+            Bicycle BMX = new Bicycle();
+
+            BMW.Move();
+
+            BMX.Move();
+             
+
+        }
+    }
+}
